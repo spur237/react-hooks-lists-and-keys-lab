cade286b40bd9885329a386123d9ec75b0edfed4
= <=>
2022-09-28 18:04:12 +0300

    done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,6 +7,10 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((tech,id) => (
+          <span key={id}>{tech}</span>
+        ))}
+
       </div>
     </div>
   );
